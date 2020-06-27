3eebd51a4daa89b537584631aa3c156ebe378c84
Ray Chen <tankimarshal2@gmail.com>
2020-06-27 19:10:15 +0100

    Add files via Git

--- client1.py
+++
@@ -1,9 +0,0 @@
-import socket
-HOST = "127.0.0.1"
-PORT = 5555
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b"Goodbye")
-    data = s.recv(1024)
-print("Received:", data.decode())
